9bc9786f292ef312e3bfd087fbcba70e96692933
Caleb Cox <calebcox314@gmail.com>
2015-10-07 14:59:10 -0500

    List requests and support creating new ones

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+const Prayers = new Mongo.Collection('prayers');
+
 if (Meteor.isClient) {
+  // This code only runs on the client
+  var app = angular.module('im-praying', ['angular-meteor', 'ngMaterial', 'angularMoment']);
+
+  app.controller('ImPrayingCtrl', function($meteor) {
+    // Initialize the scope variables
+    this.request = '';
+    this.prayers = $meteor.collection(Prayers);
+
+    // Create a new prayer request
+    this.createPrayer = function() {
+      this.prayers.push({
+        author: 1,
+        content: this.request,
+        timestamp: new Date(),
+      });
+
+      // Clear the request in preparation for creating the next one
+      this.request = '';
+    };
+  });
 }
 
 if (Meteor.isServer) {
